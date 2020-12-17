a1502ef40f697a76b03f9c5193cb23e70b905043
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2020-12-17 02:31:47 +0200

    Add files via upload

---
+++ PickUpEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpEffect : MonoBehaviour
+{
+    private AudioSource EffectAS;
+    // Start is called before the first frame update
+    void Start() {
+        EffectAS = GameObject.Find("EffectsManager").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "PlayerChar")
+        {
+            EffectAS.Play();
+            gameObject.SetActive(false);
+        }
+    }
+}
